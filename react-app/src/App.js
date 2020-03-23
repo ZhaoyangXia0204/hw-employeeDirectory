43027b9e69d5483fff93fdd9882ac4e07fe9836f
Zhaoyang Xia <zx70@scarletmail.rutgers.edu>
2020-03-22 20:36:10 -0400

    sort

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import employees from './employee.json';
+import Wrapper from './components/wrapper';
+import EmployeeTable from './components/employeeTable';
+import Title from './components/title';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    employees
+  };
+  compareBy(key) {
+    return function (a, b) {
+      if (a[key] < b[key]) return -1;
+      if (a[key] > b[key]) return 1;
+      return 0;
+    };
+  }
+ 
+  sortBy(key) {
+    let arrayCopy = [...this.state.employees];
+    arrayCopy.sort(this.compareBy(key));
+    this.setState({employees: arrayCopy});
+  }
+
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>The Employee List</Title>
+        <table className="table">
+          <thead>
+            <tr>
+              <th onClick ={() => this.sortBy('id')}  scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Department</th>
+              <th scope="col">Role</th>
+              <th scope="col">Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.employees.map(employee => (
+              <EmployeeTable
+                id={employee.id}
+                key={employee.id}
+                name={employee.name}
+                department={employee.department}
+                role={employee.role}
+                email={employee.email}
+              />
+            ))}
+          </tbody>
+
+
+        </table>
+
+
+
+      </Wrapper>
+
+    )
+  }
+
 }
 
 export default App;
---
+++ react-app/src/components/employeeTable/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+//import "./style.css";
+
+function EmployeeTable(props) {
+    return (
+
+        <tr>
+            <th scope="row" key={props.key}>{props.id}</th>
+            <td>
+                {props.name}
+            </td>
+            <td>
+                {props.department}
+            </td>
+            <td>
+                {props.role}
+            </td>
+            <td>
+                {props.email}
+            </td>
+        </tr>
+
+    );
+}
+
+export default EmployeeTable;
