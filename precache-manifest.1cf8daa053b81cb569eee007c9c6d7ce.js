b2e3f48163a9cca678c339fdea556d092bcdc5ea
Zhaoyang Xia <zx70@scarletmail.rutgers.edu>
2020-03-23 17:20:12 -0400

    Updates

---
+++ precache-manifest.1cf8daa053b81cb569eee007c9c6d7ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976a43e2dfb5bf7c4b44fa2065924551",
+    "url": "/index.html"
+  },
+  {
+    "revision": "add4aea930ff2d4634f9",
+    "url": "/static/css/main.9865c041.chunk.css"
+  },
+  {
+    "revision": "ccc64ed1c13cdf84cbe2",
+    "url": "/static/js/2.52a6cd58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.52a6cd58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add4aea930ff2d4634f9",
+    "url": "/static/js/main.61b3feef.chunk.js"
+  },
+  {
+    "revision": "e09b3a133424f3eaaad2",
+    "url": "/static/js/runtime-main.d7a593f2.js"
+  }
+]);
